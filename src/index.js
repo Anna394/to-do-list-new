ca079c7ce9ffab4a7c3e6a4e25932e092c9d1f5e
Anna Petryaeva <flo.and.mik@yandex.ru>
2025-02-04 19:46:56 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,105 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import NewTaskForm from "./components/NewTaskForm.js";
+import TaskList from "./components/TaskList.js";
+import Footer from "./components/Footer";
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const arrayTasks = [
+  { id: 1, description: "Completed task", done: true, created: Date.now() },
+  { id: 2, description: "Editing task", done: false, created: Date.now() },
+  { id: 3, description: "Active task", done: false, created: Date.now() }
+]
+
+class AppTodo extends Component {
+
+  state = {
+    tasks: arrayTasks,
+    allTasks: arrayTasks,
+    activeFilter: "All",
+  };
+
+  addTask = (description) => {
+    const newTask = {
+      id: Date.now(),
+      description,
+      done: false,
+      created: Date.now()
+    };
+    this.setState({ tasks: [...this.state.tasks, newTask],
+                  allTasks: [...this.state.allTasks, newTask]});
+  };
+
+  toggleTaskStatus = (id) => {
+    const updatedTasks = this.state.tasks.map(task =>
+      task.id === id ? { ...task, done: !task.done } : task
+    );
+    this.setState({ tasks: updatedTasks, allTasks: updatedTasks});
+  };
+
+  deleteTask = (id) => {
+    const updatedTasks = this.state.tasks.filter(task => task.id !== id);
+    this.setState({ tasks: updatedTasks, allTasks: updatedTasks});
+  };
+
+  clearCompleted = () => {
+    const activeTasks = this.state.tasks.filter(task => !task.done);
+    this.setState({ tasks: activeTasks, allTasks: activeTasks});
+  };
+
+  onActive = () => {
+    this.setState(() => ({
+      tasks: this.state.allTasks.filter(task => !task.done),
+      activeFilter: "Active"
+    }));
+  };
+  
+
+  onComplited = () => {
+    this.setState(() => ({
+      tasks: this.state.allTasks.filter(task => task.done),
+      activeFilter: "Completed"
+    }));
+  };
+
+  onAll = () => {
+    this.setState({tasks:this.state.allTasks,
+      activeFilter: "All"
+    })
+  }
+
+
+
+  render() {
+    return (
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <NewTaskForm onAddTask={this.addTask} />
+        </header>
+        <section className="main">
+          <TaskList
+            tasks={this.state.tasks}
+            onToggle={this.toggleTaskStatus}
+            onDelete={this.deleteTask}
+          />
+        </section>
+        <Footer
+          onClearCompleted={this.clearCompleted}
+          onActive={this.onActive}
+          onComplited={this.onComplited}
+          onAll={this.onAll}
+          selected= {this.state.activeFilter}
+          activeCount = {this.state.allTasks.filter(task => !task.done).length}
+        />
+      </section>
+    );
+  }
+}
+
+export default AppTodo;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppTodo />);
+
